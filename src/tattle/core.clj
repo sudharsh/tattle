9c505eaa1caa5a05a89ad3d91ee9eaced73e85db
Sudharshan S <sudharsh@gmail.com>
2013-12-02 22:47:32 +0530

    Add a way of executing handlers
    Handlers are functions that take in the input and return a json that is returned back over the wire

--- src/tattle/core.clj
+++ src/tattle/core.clj
@@ -1,23 +1,16 @@
 (ns tattle.core
   (:require [tattle.socket.server :as server]
             [clojure.java.shell :as sh]
-            [clojure.java.io :as io]
-            [clojure.string :refer [trim]]
-            [taoensso.timbre :as timbre :refer :all])
-  (:import [java.util.concurrent Executors]
-           [java.io  DataOutputStream PrintWriter StringWriter Writer BufferedReader InputStreamReader BufferedInputStream OutputStreamWriter]))
+            [clojure.string :as string]
+            [taoensso.timbre :as timbre :refer :all]))
 
-(defn getstack [t]
-  (let [res (StringWriter.)
-        pw  (PrintWriter. res)
-        trace (.printStackTrace t pw)]
-    (.toString res)))
-  
+(def nodes (ref {}))
 
+(defn uptime [_]
+  (string/trim (:out (sh/sh "uptime"))))
 
-(defn echo-server [in out]
-  (let [ps (PrintWriter. out true)
-        _ (.close ps)]
-    (.print ps "zGooo")))
-  
+(defn list-nodes [_]
+  (keys @nodes))
 
+(server/add-handler :uptime uptime)
+(server/add-handler :list-nodes list-nodes)
--- src/tattle/socket/server.clj
+++ src/tattle/socket/server.clj
@@ -1,12 +1,15 @@
-
 (ns tattle.socket.server
   (:require  [clojure.java.shell :as sh]
              [clojure.java.io :as io]
-             [clojure.string :refer [trim]])
+             [clojure.string :as string]
+             [cheshire.core :as json])
   (:import [java.net ServerSocket Socket SocketException]
-           [java.io DataOutputStream InputStreamReader PrintWriter StringWriter OutputStreamWriter]))
+           [java.io DataOutputStream InputStreamReader InputStream
+            PrintWriter StringWriter OutputStreamWriter]))
 
 (defrecord Servers [server connections closed])
+
+(def handlers (ref {}))
 
 (defn- getstack [t]
   (let [res (StringWriter.)
@@ -17,25 +20,34 @@
 (defn- on-thread [f]
   (doto (Thread. f) .start))
 
-(defn- close [socket]
+(defn- close [^Socket socket]
   (when-not (.isClosed socket)
     (doto socket
       (.close))))
 
-(defn- accept [serversock connections closed handler]
+(defn- read-stream [^InputStream ins]
+  (let [br (io/reader ins)]
+    (string/join "\n" (line-seq br))))
+
+(defn- accept [serversock connections closed]
   (let [s (.accept serversock)
         ins (.getInputStream s)
-        outs (.getOutputStream s)]
+        outs (.getOutputStream s)
+        ps (PrintWriter. outs true)]
     (on-thread #(do
                   (dosync (commute connections conj s))
                   (try
-                    (handler ins outs)
+                    (doto ps
+                      (.print (json/generate-string
+                               (execute-handler handlers ins outs)))
+                      (.checkError))
                     (catch SocketException e))
                   (close s)
                   (dosync (commute connections disj s)
                           (swap! closed inc))))))
 
-(defn create-server [port handler]
+;; Pub functions
+(defn create-server [port]
   "Creates a server on port exec'ing func with an input-stream
   and an output stream"
   (let [ss (ServerSocket. port)
@@ -43,29 +55,44 @@
         closed (atom 0)]
     (on-thread #(when-not (.isClosed ss)
                   (try
-                    (accept ss connections closed handler)
+                    (accept ss connections closed)
                     (catch SocketException e (println (str "Foo: " (getstack e)))))
                   (recur)))
     (Servers. ss connections closed)))
-    
+
 (defn close-server [server]
+  "Closes a server. Shuts down all connections"
   (doseq [s @(:connections server)]
     (close s))
   (dosync (ref-set (:connections server) #{}))
   (close (:server server)))
 
 (defn count-connections [server]
+  "Get the number of open connections to this agent"
   (count @(:connections server)))
 
 (defn closed-connections [server]
+  "Get the number of closed connections"
   @(:closed server))
 
-(defn get-uptime [ins outs]
-  (try
-    (let [ps (PrintWriter. outs true)
-          output (trim (:out (sh/sh "uptime")))]
-      (doto ps
-        (.print output)
-        (.checkError)))
-    (catch Exception e (println (getstack e)))))
+(defn add-handler [command fun]
+  "Add a handler for a given command"
+  (dosync (commute handlers assoc command fun)))
 
+(defn remove-handler [command]
+  "Remove a handler"
+  (dosync (commute handlers dissoc command)))
+
+;; Execute a handler
+(defn- execute-handler [handlers ins outs]
+  (try
+    (let [input (json/parse-string (read-stream ins) true)
+          command (keyword (:command input))
+          f (get @handlers command)]
+      (if (nil? f)
+        {:error  (str "invalid command" command)}
+        {:response (f (:args input))}))
+    (catch Exception e
+      {:error      "internal"
+       :stacktrace (getstack e)
+       :exception  (str e)})))
