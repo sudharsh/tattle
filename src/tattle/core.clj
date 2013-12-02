ad39aeca5d5c3b87d4bf3f82ae4e35a31b95efe1
Sudharshan S <sudharsh@gmail.com>
2013-12-03 00:20:55 +0530

    Add a main function

--- src/tattle/core.clj
+++ src/tattle/core.clj
@@ -2,11 +2,15 @@
   (:require [tattle.socket.server :as server]
             [clojure.java.shell :as sh]
             [clojure.string :as string]
-            [taoensso.timbre :as timbre :refer :all]))
+            [taoensso.timbre :as timbre :refer :all])
+  (:gen-class))
 
-(def nodes (ref (assoc {} (keyword
-                           (.getHostAddress (java.net.InetAddress/getLocalHost)))
-                       "up")))
+
+(def nodes (ref
+            (assoc {}
+              (keyword
+               (.getHostAddress (java.net.InetAddress/getLocalHost)))
+              "up")))
 
 (defn uptime [_]
   (string/trim (:out (sh/sh "uptime"))))
@@ -14,5 +18,10 @@
 (defn get-nodes [_]
   @nodes)
 
-(server/add-handler :uptime uptime)
-(server/add-handler :nodes get-nodes)
+(defn -main [& args]
+  (info "Adding handlers and some such")
+  (server/add-handler :uptime uptime)
+  (server/add-handler :nodes get-nodes)
+  (info "Starting agent")
+  (server/create-server 6000)
+  (info "Listening on port 6000"))
